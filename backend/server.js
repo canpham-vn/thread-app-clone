d19f7d66b24fc026f9f4fe97b9a8a056c24cc279
canpham-vn <canpham0610@gmail.com>
2024-04-21 22:36:41 +0700

    Handle route signup user

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import connectDB from "./db/connectDB.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -8,6 +11,13 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser()); // To get cookies from the request and set cookies inside response
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
