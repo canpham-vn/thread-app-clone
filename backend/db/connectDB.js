2b7268b8168cc46c4d532ca5dc3ea8e219bf316f
canpham-vn <canpham0610@gmail.com>
2024-04-21 21:29:06 +0700

    Connect MongoDB

---
+++ backend/db/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      // To avoid warnings in the console
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB is connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
