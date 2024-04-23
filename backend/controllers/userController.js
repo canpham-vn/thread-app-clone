543e6f072f140b1133cdf7c3322fd8e2683799d5
canpham-vn <canpham0610@gmail.com>
2024-04-23 21:45:02 +0700

    Handle follow and unfollow user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,4 +79,40 @@ const logoutUser = async (req, res) => {
   }
 };
 
-export { signupUser, loginUser, logoutUser };
+const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id) {
+      return res
+        .status(400)
+        .json({ message: "You cannot follow/unfollow yourself" });
+    }
+
+    if (!userToModify || !currentUser) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // Unfollow user
+      // Modify current user following, modify followers of userToModify
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error: " + error.message);
+  }
+};
+
+export { signupUser, loginUser, logoutUser, followUnfollowUser };
