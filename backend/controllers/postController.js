ebd32a7c43650e93b77a38ab3d3ac0f2aedcda84
canpham-vn <canpham0610@gmail.com>
2024-04-26 20:29:13 +0700

    Handle get post route

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,6 +1,22 @@
 import User from "../models/userModel.js";
 import Post from "../models/postModel.js";
 
+const getPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(400).json({ message: "Post not found" });
+    }
+
+    res.status(200).json({ post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error: " + error.message);
+  }
+};
+
 const createPost = async (req, res) => {
   try {
     const { postedBy, text, img } = req.body;
@@ -37,4 +53,4 @@ const createPost = async (req, res) => {
   }
 };
 
-export { createPost };
+export { createPost, getPost };
