442e53b158070b99a2be7647aa7572023b382818
canpham-vn <canpham0610@gmail.com>
2024-04-27 23:30:08 +0700

    Handle reply post route

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -102,4 +102,32 @@ const likeUnlikePost = async (req, res) => {
   }
 };
 
-export { createPost, getPost, deletePost, likeUnlikePost };
+const replyToPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePic = req.user.profilePic;
+    const username = req.user.username;
+
+    if (!text) {
+      return res.status(400).json({ message: "Text is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const reply = { userId, text, userProfilePic, username };
+    post.replies.push(reply);
+    await post.save();
+
+    res.status(200).json({ message: "Reply added successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error: " + error.message);
+  }
+};
+
+export { createPost, getPost, deletePost, likeUnlikePost, replyToPost };
