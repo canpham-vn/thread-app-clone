24babfd9596d403402842ed7f8169929db84f09a
canpham-vn <canpham0610@gmail.com>
2024-04-30 21:49:05 +0700

    Handle signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,18 @@ import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import { useRecoilValue } from "recoil";
+import userAtom from "./atoms/userAtom";
 
 function App() {
+  const user = useRecoilValue(userAtom);
+
   return (
     <Container maxW='620px'>
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/auth' element={<AuthPage />} />
+        <Route path='/' element={user ? <HomePage /> : <AuthPage />} />
+        <Route path='/auth' element={!user ? <AuthPage /> : <HomePage />} />
         <Route path='/:username' element={<UserPage />} />
         <Route path='/:username/post/:postId' element={<PostPage />} />
       </Routes>
--- frontend/src/components/SignupCard.jsx
+++ frontend/src/components/SignupCard.jsx
@@ -18,10 +18,45 @@ import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { useSetRecoilState } from "recoil";
 import authScreenAtom from "../atoms/authAtom";
+import useShowToast from "../hooks/useShowToast";
+import userAtom from "../atoms/userAtom";
 
 const SignupCard = () => {
   const [showPassword, setShowPassword] = useState(false);
   const setAuthScreen = useSetRecoilState(authScreenAtom);
+  const setUser = useSetRecoilState(userAtom);
+  const [inputs, setInputs] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const showToast = useShowToast();
+
+  const handleSignup = async () => {
+    console.log(inputs);
+    try {
+      const res = await fetch("/api/users/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+
+        return;
+      }
+
+      localStorage.setItem("user-threads", JSON.stringify(data));
+      setUser(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Flex align={"center"} justify={"center"}>
@@ -42,24 +77,48 @@ const SignupCard = () => {
               <Box>
                 <FormControl isRequired>
                   <FormLabel>Full name</FormLabel>
-                  <Input type='text' />
+                  <Input
+                    type='text'
+                    onChange={(e) =>
+                      setInputs({ ...inputs, name: e.target.value })
+                    }
+                    value={inputs.name}
+                  />
                 </FormControl>
               </Box>
               <Box>
                 <FormControl isRequired>
                   <FormLabel>Username</FormLabel>
-                  <Input type='text' />
+                  <Input
+                    type='text'
+                    onChange={(e) =>
+                      setInputs({ ...inputs, username: e.target.value })
+                    }
+                    value={inputs.username}
+                  />
                 </FormControl>
               </Box>
             </HStack>
             <FormControl isRequired>
               <FormLabel>Email address</FormLabel>
-              <Input type='email' />
+              <Input
+                type='email'
+                onChange={(e) =>
+                  setInputs({ ...inputs, email: e.target.value })
+                }
+                value={inputs.email}
+              />
             </FormControl>
             <FormControl isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? "text" : "password"} />
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, password: e.target.value })
+                  }
+                  value={inputs.password}
+                />
                 <InputRightElement h={"full"}>
                   <Button
                     variant={"ghost"}
@@ -81,6 +140,7 @@ const SignupCard = () => {
                 _hover={{
                   bg: useColorModeValue("gray.700", "gray.800"),
                 }}
+                onClick={handleSignup}
               >
                 Sign up
               </Button>
