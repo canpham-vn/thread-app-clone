d6d730bd8c2a8ff06a4aaf962dc962e662943c7f
canpham-vn <canpham0610@gmail.com>
2024-05-07 22:20:46 +0700

    Update limit body request

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser()); // To get cookies from the request and set cookies inside response
 
