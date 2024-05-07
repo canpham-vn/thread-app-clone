04f8803e4521e27076a0db908046c7f858a7c647
canpham-vn <canpham0610@gmail.com>
2024-05-07 22:21:11 +0700

    Handle upload post img to cloudinary

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,6 +1,8 @@
 import User from "../models/userModel.js";
 import Post from "../models/postModel.js";
 
+import { v2 as cloudinary } from "cloudinary";
+
 const getPost = async (req, res) => {
   try {
     const { id } = req.params;
@@ -40,7 +42,8 @@ const getFeedPosts = async (req, res) => {
 
 const createPost = async (req, res) => {
   try {
-    const { postedBy, text, img } = req.body;
+    const { postedBy, text } = req.body;
+    let { img } = req.body;
 
     if (!postedBy || !text) {
       return res
@@ -63,6 +66,11 @@ const createPost = async (req, res) => {
       return res
         .status(400)
         .json({ error: `Text must be less than ${maxLength} characters` });
+    }
+
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader.upload(img);
+      img = uploadedResponse.secure_url;
     }
 
     const newPost = new Post({ postedBy, text, img });
