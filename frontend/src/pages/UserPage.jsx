56ff54d2c0f963158f8137e570d687245f156604
canpham-vn <canpham0610@gmail.com>
2024-05-08 22:55:03 +0700

    Handle loading User Page

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -3,14 +3,17 @@ import UserHeader from "../components/UserHeader";
 import UserPost from "../components/UserPost";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
   const { username } = useParams();
   const showToast = useShowToast();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
+      setIsLoading(true);
       try {
         const res = await fetch(`/api/users/profile/${username}`);
         const data = await res.json();
@@ -22,13 +25,22 @@ const UserPage = () => {
         setUser(data);
       } catch (error) {
         showToast("Error", error, "error");
+      } finally {
+        setIsLoading(false);
       }
     };
 
     getUser();
   }, [showToast, username]);
 
-  if (!user) return null;
+  if (!user && isLoading) {
+    return (
+      <Flex justifyContent='center'>
+        <Spinner size='xl' />;
+      </Flex>
+    );
+  }
+  if (!user && !isLoading) return <h1>User not found</h1>;
 
   return (
     <>
