464f27e47c05fb844e127cd1779c60d626ef8767
canpham-vn <canpham0610@gmail.com>
2024-05-09 22:31:44 +0700

    Update text loading login

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -107,7 +107,7 @@ const LoginCard = () => {
             </FormControl>
             <Stack spacing={10} pt={2}>
               <Button
-                loadingText='Submitting'
+                loadingText='Logging in'
                 size='lg'
                 bg={useColorModeValue("gray.600", "gray.700")}
                 color={"white"}
