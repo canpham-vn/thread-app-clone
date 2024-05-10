e100c4f25370d8bfb8031f6512c064de4c2c7862
canpham-vn <canpham0610@gmail.com>
2024-05-10 23:19:24 +0700

    Update get user by query or username

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,14 +3,25 @@ import { v2 as cloudinary } from "cloudinary";
 
 import User from "../models/userModel.js";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
+import mongoose from "mongoose";
 
 const getUserProfile = async (req, res) => {
-  const { username } = req.params;
+  const { query } = req.params;
 
   try {
-    const user = await User.findOne({ username })
-      .select("-password")
-      .select("-updatedAt");
+    let user;
+
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      // query is username
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
 
     if (!user) {
       return res.status(400).json({ error: "User not found" });
