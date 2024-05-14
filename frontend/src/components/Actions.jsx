f9799c1fc997aa199bf3454e0ce1160ca718266c
canpham-vn <canpham0610@gmail.com>
2024-05-14 22:33:21 +0700

    Handle like post

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,6 +1,50 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
 
-const Actions = ({ liked, setLiked }) => {
+const Actions = ({ post: post_ }) => {
+  const user = useRecoilValue(userAtom);
+  const [post, setPost] = useState(post_);
+  const [liked, setLiked] = useState(post_.likes.includes(user?._id));
+  const showToast = useShowToast();
+
+  const handleLikeAndUnlike = async () => {
+    if (!user) {
+      return showToast(
+        "Error",
+        "You must be logged in to like a post",
+        "error"
+      );
+    }
+    try {
+      const res = await fetch(`/api/posts/like/${post._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+
+      console.log(data);
+
+      if (data.error) {
+        showToast("Error", data.error, "error");
+      }
+
+      if (!liked) {
+        setPost({ ...post, likes: [...post.likes, user._id] });
+      } else {
+        setPost({ ...post, likes: post.likes.filter((id) => id !== user._id) });
+      }
+
+      setLiked(!liked);
+    } catch (error) {
+      showToast("Error", error, "error");
+    }
+  };
+
   return (
     <Flex flexDirection='column'>
       <Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
@@ -13,7 +57,7 @@ const Actions = ({ liked, setLiked }) => {
           viewBox='0 0 24 22'
           width='20'
           cursor='pointer'
-          onClick={() => setLiked((prev) => !prev)}
+          onClick={handleLikeAndUnlike}
         >
           <path
             d='M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z'
@@ -88,6 +132,16 @@ const Actions = ({ liked, setLiked }) => {
             strokeWidth='2'
           ></polygon>
         </svg>
+      </Flex>
+
+      <Flex gap={2} alignItems='center'>
+        <Text color='gray.light' fontSize='sm'>
+          {post.replies.length} replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius='full' bg='gray.light'></Box>
+        <Text color='gray.light' fontSize='sm'>
+          {post.likes.length} likes
+        </Text>
       </Flex>
     </Flex>
   );
