d7cb30f1fe541ab50d0fce4743ee42a72720dcd2
canpham-vn <canpham0610@gmail.com>
2024-05-15 21:49:06 +0700

    Handle reply post

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,4 +1,19 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import { useRecoilValue } from "recoil";
@@ -9,7 +24,11 @@ const Actions = ({ post: post_ }) => {
   const [post, setPost] = useState(post_);
   const [liked, setLiked] = useState(post_.likes.includes(user?._id));
   const [isLiking, setIsLiking] = useState(false);
+  const [reply, setReply] = useState("");
+  const [isReplying, setIsReplying] = useState(false);
+
   const showToast = useShowToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleLikeAndUnlike = async () => {
     if (!user) {
@@ -31,8 +50,6 @@ const Actions = ({ post: post_ }) => {
       });
       const data = await res.json();
 
-      console.log(data);
-
       if (data.error) {
         showToast("Error", data.error, "error");
       }
@@ -48,6 +65,43 @@ const Actions = ({ post: post_ }) => {
       showToast("Error", error, "error");
     } finally {
       setIsLiking(false);
+    }
+  };
+
+  const handleReply = async () => {
+    if (!user) {
+      return showToast(
+        "Error",
+        "You must be logged in to reply a post",
+        "error"
+      );
+    }
+
+    if (isReplying) return;
+    setIsReplying(true);
+    try {
+      const res = await fetch(`/api/posts/reply/${post._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: reply }),
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        showToast("Error", data.error, "error");
+      }
+
+      setPost({ ...post, replies: [...post.replies, data.reply] });
+      onClose();
+      showToast("Success", "Reply posted successfully", "success");
+      setReply("");
+    } catch (error) {
+      showToast("Error", error, "error");
+    } finally {
+      setIsReplying(false);
     }
   };
 
@@ -81,6 +135,7 @@ const Actions = ({ post: post_ }) => {
           viewBox='0 0 24 24'
           width='20'
           cursor='pointer'
+          onClick={onOpen}
         >
           <title>Comment</title>
           <path
@@ -92,52 +147,8 @@ const Actions = ({ post: post_ }) => {
           ></path>
         </svg>
 
-        <svg
-          aria-label='Repost'
-          color='currentColor'
-          fill='currentColor'
-          height='20'
-          role='img'
-          viewBox='0 0 24 24'
-          width='20'
-          cursor='pointer'
-        >
-          <title>Repost</title>
-          <path
-            fill=''
-            d='M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z'
-          ></path>
-        </svg>
-
-        <svg
-          aria-label='Share'
-          color=''
-          fill='rgb(243, 245, 247)'
-          height='20'
-          role='img'
-          viewBox='0 0 24 24'
-          width='20'
-          cursor='pointer'
-        >
-          <title>Share</title>
-          <line
-            fill='none'
-            stroke='currentColor'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            x1='22'
-            x2='9.218'
-            y1='3'
-            y2='10.083'
-          ></line>
-          <polygon
-            fill='none'
-            points='11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334'
-            stroke='currentColor'
-            strokeLinejoin='round'
-            strokeWidth='2'
-          ></polygon>
-        </svg>
+        <RepostSVG />
+        <ShareSVG />
       </Flex>
 
       <Flex gap={2} alignItems='center'>
@@ -149,8 +160,92 @@ const Actions = ({ post: post_ }) => {
           {post.likes.length} likes
         </Text>
       </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <Input
+                placeholder='Reply goes here...'
+                value={reply}
+                onChange={(e) => setReply(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme='blue'
+              mr={3}
+              size='sm'
+              isLoading={isReplying}
+              onClick={handleReply}
+            >
+              Reply
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
 
 export default Actions;
+
+const RepostSVG = () => {
+  return (
+    <svg
+      aria-label='Repost'
+      color='currentColor'
+      fill='currentColor'
+      height='20'
+      role='img'
+      viewBox='0 0 24 24'
+      width='20'
+      cursor='pointer'
+    >
+      <title>Repost</title>
+      <path
+        fill=''
+        d='M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z'
+      ></path>
+    </svg>
+  );
+};
+
+const ShareSVG = () => {
+  return (
+    <svg
+      aria-label='Share'
+      color=''
+      fill='rgb(243, 245, 247)'
+      height='20'
+      role='img'
+      viewBox='0 0 24 24'
+      width='20'
+      cursor='pointer'
+    >
+      <title>Share</title>
+      <line
+        fill='none'
+        stroke='currentColor'
+        strokeLinejoin='round'
+        strokeWidth='2'
+        x1='22'
+        x2='9.218'
+        y1='3'
+        y2='10.083'
+      ></line>
+      <polygon
+        fill='none'
+        points='11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334'
+        stroke='currentColor'
+        strokeLinejoin='round'
+        strokeWidth='2'
+      ></polygon>
+    </svg>
+  );
+};
