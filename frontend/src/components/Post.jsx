caf31f03e1280f81a0cde97b7dfb43fbb845fabd
canpham-vn <canpham0610@gmail.com>
2024-05-18 21:12:43 +0700

    Handle delete post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,21 +1,45 @@
 import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
-import { BsThreeDots } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import Actions from "./Actions";
 import { useEffect, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import { formatDistanceToNow } from "date-fns";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
 
 const Post = ({ post, postedBy }) => {
   const [user, setUser] = useState(null);
 
   const showToast = useShowToast();
-
+  const currentUser = useRecoilValue(userAtom);
   const navigate = useNavigate();
 
   const navigateToUserPage = (e) => {
     e.preventDefault();
     navigate(`/${user.username}`);
+  };
+
+  const handleDeletePost = async (e) => {
+    try {
+      e.preventDefault();
+      if (!confirm("Are you sure you want to delete this post?")) return;
+
+      const res = await fetch(`/api/posts/${post._id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+
+      showToast("Success", "Post deleted", "success");
+    } catch (error) {
+      showToast("Error", error, "error");
+    }
   };
 
   useEffect(() => {
@@ -108,7 +132,10 @@ const Post = ({ post, postedBy }) => {
               <Text fontSize='xs' color='gray.light' w={36} textAlign='right'>
                 {formatDistanceToNow(new Date(post.createdAt))} ago
               </Text>
-              <BsThreeDots />
+
+              {currentUser?._id === user._id && (
+                <DeleteIcon size={20} onClick={handleDeletePost} />
+              )}
             </Flex>
           </Flex>
           <Flex fontSize='sm'>{post.text}</Flex>
